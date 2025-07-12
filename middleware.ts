6382f422a7cc9ef7f36dfdb516809249c99988b8
YeshviC <147177326+YeshviC@users.noreply.github.com>
2025-07-12 18:32:17 +0530

    repo change

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-import getOrCreateDB from './models/server/dbSetup'
-import getOrCreateStorage from './models/server/storageSetup'
+import getOrCreateDB from './src/models/server/dbSetup'
+import getOrCreateStorage from './src/models/server/storageSetup'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
